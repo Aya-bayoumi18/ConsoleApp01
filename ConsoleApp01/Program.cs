ec10e147f754cf38074ab1294433dceb3d03577c
Aya-bayoumi18 <ayaasar99@gmail.com>
2025-07-19 18:38:57 +0300

    Add project files.

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int C = 10;
+            int R = 34;
+            Console.WriteLine(C + R);
+
+        }
+
+    }}
